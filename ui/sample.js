3162dbef172b5d6147fde69cb6d21748ca1da2d1
redwrasse <redwrasse@users.noreply.github.com>
2021-01-16 00:54:17 -0800

    mock table

--- ui/sample.js
+++ ui/sample.js
@@ -26,3 +26,54 @@ const sample = {
     query: "wh",
     completions: [completion1, completion2]
 };
+
+function buildTable(labels, objects, container) {
+  var table = document.createElement('table');
+  var thead = document.createElement('thead');
+  var tbody = document.createElement('tbody');
+
+  var theadTr = document.createElement('tr');
+  for (var i = 0; i < labels.length; i++) {
+    var theadTh = document.createElement('th');
+    theadTh.innerHTML = labels[i];
+    theadTr.appendChild(theadTh);
+  }
+  thead.appendChild(theadTr);
+  table.appendChild(thead);
+
+  for (j = 0; j < objects.length; j++) {
+    var tbodyTr = document.createElement('tr');
+    for (k = 0; k < labels.length; k++) {
+      var tbodyTd = document.createElement('td');
+      tbodyTd.innerHTML = objects[j][labels[k].toLowerCase()];
+      tbodyTr.appendChild(tbodyTd);
+    }
+    tbody.appendChild(tbodyTr);
+  }
+  table.appendChild(tbody);
+
+  container.appendChild(table);
+}
+
+var labels1 = ['ID', 'Name'];
+var objects1 = [
+  {"id": "1", 'name': "richard"},
+  {"id": "2", 'name': "santos"}
+];
+
+var labels2 = ['ID', 'NOME'];
+var objects2 = [
+  {"id": "1", 'nome': "richard"},
+  {"id": "2", 'nome': "adriana"}
+];
+
+var labels3 = ['Completion', 'Likelihood', 'Terms'];
+var objects3 = [
+  {"completion": "which", "likelihood": "21.90%"},
+ {"completion": "what", "likelihood": "17.29%"},
+    {"completion": "who, ", "likelihood": "13.05%"},
+    {"completion": "while", "likelihood": "11.12%"}
+];
+
+
+buildTable(labels3, objects3, document.getElementById('results'));
